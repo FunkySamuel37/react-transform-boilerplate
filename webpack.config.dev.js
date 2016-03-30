60bd4796601e2779117f4102208b95c2f965b903
FunkySamuel37 <masterzhiwei@gmail.com>
2016-03-30 22:15:39 +0800

    prework , add packages, fix webpack devTool syntax error

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   // or devtool: 'eval' to debug issues with compiled output:
-  devtool: 'cheap-module-eval-source-map',
+  devtool: '#cheap-module-eval-source-map',
   entry: [
     // necessary for hot reloading with IE:
     'eventsource-polyfill',
@@ -26,6 +26,10 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!autoprefixer!sass' 
     }]
   }
 };
